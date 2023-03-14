a469f3e14a3310d9c29f73ae0ab69d676da3ce95
omer-creator <afzalmuhammadomer@gmail.com>
2023-03-14 19:25:35 +0500

    Add mobilemenu.js

--- Javacsript/mobile-menu.js
+++ Javacsript/mobile-menu.js
@@ -1,6 +1,33 @@
 const menuToggle = document.querySelector(".Sidebar-class");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuItems = document.querySelectorAll(".mobile-menu-items li a");
+const closeBtn = document.querySelector('.close-btn');
+const header = document.querySelector('header');
 
 menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+    menuItems.forEach((link) => {
+      link.classList.remove("active");
+    });
+    item.classList.add("active");
+  });
+});
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+  menuItems.forEach((link) => {
+    link.classList.remove("active");
+  });
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
 });

2b05d30d2056d1af9f457de17c73aebbcefa0505
Abenezer Assefa <94551524+AbenezerAssefa@users.noreply.github.com>
2023-03-14 19:58:08 +0300

    Update mobile-menu.js

--- Javacsript/mobile-menu.js
+++ Javacsript/mobile-menu.js
@@ -8,12 +8,7 @@ menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
 });
 
-menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    mobileMenu.classList.remove("active");
-    menuItems.forEach((link) => {
-      link.classList.remove("active");
-    });
+
     item.classList.add("active");
   });
 });

1949ffad213073ed7e57164bb49056a47f310485
omer-creator <afzalmuhammadomer@gmail.com>
2023-03-17 11:57:25 +0500

    Fic eslint errors

--- Javacsript/mobile-menu.js
+++ Javacsript/mobile-menu.js
@@ -1,27 +1,25 @@
-const menuToggle = document.querySelector(".Sidebar-class");
-const mobileMenu = document.querySelector(".mobile-menu");
-const menuItems = document.querySelectorAll(".mobile-menu-items li a");
+const menuToggle = document.querySelector('.Sidebar-class');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuItems = document.querySelectorAll('.mobile-menu-items li a');
 const closeBtn = document.querySelector('.close-btn');
-const header = document.querySelector('header');
+// const header = document.querySelector('header');
 
-menuToggle.addEventListener("click", () => {
-  mobileMenu.classList.toggle("active");
+menuToggle.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
 });
 
 menuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    mobileMenu.classList.remove("active");
+  item.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
     menuItems.forEach((link) => {
-      link.classList.remove("active");
+      link.classList.remove('active');
     });
-    item.classList.add("active");
+    item.classList.add('active');
   });
 });
-closeBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("active");
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
   menuItems.forEach((link) => {
-    link.classList.remove("active");
+    link.classList.remove('active');
   });
 });
-
-

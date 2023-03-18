6e2c7b77fe0dc26cddf0c258157f9e736469dd52
Abenezer Assefa <94551524+AbenezerAssefa@users.noreply.github.com>
2023-03-18 14:28:58 +0300

    Update mobile-contact-validation.js

--- Javacsript/mobile-contact-validation.js
+++ Javacsript/mobile-contact-validation.js
@@ -1,15 +1,5 @@
 const emailInput = document.getElementById('email-input');
 
-function validateForm(event) {
-  event.preventDefault();
-  if (emailInput.value === emailInput.value.toLowerCase()) {
-    event.target.submit();
-  } else {
-    const errorDiv = document.createElement('div');
-    errorDiv.innerText = 'Please enter your email address in lowercase.';
-    errorDiv.classList.add('error-message');
-    event.target.appendChild(errorDiv);
-  }
-}
+
 
 document.querySelector('form').addEventListener('submit', validateForm);

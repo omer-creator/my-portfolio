4bb63208f2c97196893a2db6a5479dc49bb1a27f
omer-creator <afzalmuhammadomer@gmail.com>
2023-03-26 01:28:44 +0500

    Fix eslint error

--- Javacsript/desktop-contact-validation.js
+++ Javacsript/desktop-contact-validation.js
@@ -1,20 +1,14 @@
-const form = document.getElementById("contact-form");
-const emailInput = document.getElementById("email-input");
-const submitButton = document.getElementById("submit-button");
-const errorMessage = document.getElementById("error-message");
+const form = document.getElementById('contact-form');
+const emailInput = document.getElementById('email-input');
+const errorMessage = document.getElementById('error-message');
 
-form.addEventListener("submit", function (event) {
+form.addEventListener('submit', (event) => {
   // Prevent the form from submitting if validation fails
   if (emailInput.value !== emailInput.value.toLowerCase()) {
     event.preventDefault();
-    errorMessage.style.display = "block";
+    errorMessage.style.display = 'block';
     emailInput.value = emailInput.value.toLowerCase();
   } else {
-    errorMessage.style.display = "none";
+    errorMessage.style.display = 'none';
   }
 });
-
-
-
-
-
